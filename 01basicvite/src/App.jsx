d1d986769e5e5446f81d5e5a899fc381483ed619
christian <christian17silveira@gmail.com>
2025-02-06 10:35:30 -0300

    1:40 react course

---
+++ 01basicvite/src/App.jsx
@@ -0,0 +1,14 @@
+import Youtube from "./Youtube"
+
+function App() {
+
+  return (
+    <>
+    <h1>Vite react app {2+2} </h1>
+    <h1>Vite react app {" "} </h1>
+    <Youtube />
+    </>
+  )
+}
+
+export default App
